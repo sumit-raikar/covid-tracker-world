ca71742eee32e078aa194be1489b03dbaba997ca
Sumit Raikar <sumitraikar@gmail.com>
2020-09-12 13:20:35 +0530

    Updates

---
+++ precache-manifest.072eb54d4aaac2aadb3e4d563d967c4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c397e1f04c0f5a33f64883da73d87609",
+    "url": "/covid-tracker-world/index.html"
+  },
+  {
+    "revision": "fd976b3ff0766293a7a5",
+    "url": "/covid-tracker-world/static/css/main.ed5ebb37.chunk.css"
+  },
+  {
+    "revision": "de6de5acacf9d750404c",
+    "url": "/covid-tracker-world/static/js/2.a443da6b.chunk.js"
+  },
+  {
+    "revision": "38d43c6ba36865fb0cc07e2539b0465a",
+    "url": "/covid-tracker-world/static/js/2.a443da6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd976b3ff0766293a7a5",
+    "url": "/covid-tracker-world/static/js/main.eef2f085.chunk.js"
+  },
+  {
+    "revision": "cf11cd7a853d87666536",
+    "url": "/covid-tracker-world/static/js/runtime-main.c6ce23c6.js"
+  }
+]);
